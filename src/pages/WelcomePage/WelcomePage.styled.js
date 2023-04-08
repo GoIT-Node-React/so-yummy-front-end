d3f8bea837f270f3ada4325077f5cff922c93c58
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-08 15:31:32 +0300

    Refactoring

--- src/pages/WelcomePage/WelcomePage.styled.js
+++ src/pages/WelcomePage/WelcomePage.styled.js
@@ -11,31 +11,33 @@ import mainBgDesktop2x from '../../images/Start/start-2xdesc.jpg';
 export const SvgWrapper = styled.div`
   display: flex;
   justify-content: center;
+
   margin-bottom: 44px;
+
   svg {
     width: 54px;
     height: 54px;
+
     @media screen and (min-width: ${theme.breakpoints[1]}) {
       width: 68px;
       height: 68px;
     }
   }
 `;
+
 export const Main = styled.div`
-  margin: 0 auto;
-  height: 100vh;
+  --background-image: url(${mainBgMobile});
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  font-family: ${({ theme }) => theme.fonts};
-  font-style: normal;
+  margin: 0 auto;
+  height: 100vh;
 
   padding: 0 35px;
 
   background-size: cover;
-  background-widh: 100wh;
   background-repeat: no-repeat;
   background-position: center;
   background-image: linear-gradient(
@@ -44,59 +46,33 @@ export const Main = styled.div`
       rgba(0, 0, 0, 0.345172) 78.98%,
       rgba(0, 0, 0, 0) 100%
     ),
-    url(${mainBgMobile});
-  background-size: cover;
-  background-repeat: no-repeat;
+    var(--background-image);
+
   @media (min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
-    background-image: linear-gradient(
-        180deg,
-        rgba(0, 0, 0, 0.6) 58.58%,
-        rgba(0, 0, 0, 0.345172) 78.98%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      url(${mainBgMobile2x});
+    --background-image: url(${mainBgMobile2x});
   }
+
   @media screen and (min-width: ${theme.breakpoints[1]}) {
+    --background-image: url(${mainBgTablet});
+
     padding: 0 135px;
-    background-image: linear-gradient(
-        180deg,
-        rgba(0, 0, 0, 0.6) 58.58%,
-        rgba(0, 0, 0, 0.345172) 78.98%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      url(${mainBgTablet});
+
     @media (min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
-      background-image: linear-gradient(
-          180deg,
-          rgba(0, 0, 0, 0.6) 58.58%,
-          rgba(0, 0, 0, 0.345172) 78.98%,
-          rgba(0, 0, 0, 0) 100%
-        ),
-        url(${mainBgTablet2x});
+      --background-image: url(${mainBgTablet2x});
     }
   }
+
   @media screen and (min-width: ${theme.breakpoints[2]}) {
-    background-image: linear-gradient(
-        180deg,
-        rgba(0, 0, 0, 0.6) 58.58%,
-        rgba(0, 0, 0, 0.345172) 78.98%,
-        rgba(0, 0, 0, 0) 100%
-      ),
-      url(${mainBgDesktop});
+    --background-image: url(${mainBgDesktop});
+
     @media (min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
-      background-image: linear-gradient(
-          180deg,
-          rgba(0, 0, 0, 0.6) 58.58%,
-          rgba(0, 0, 0, 0.345172) 78.98%,
-          rgba(0, 0, 0, 0) 100%
-        ),
-        url(${mainBgDesktop2x});
+      --background-image: url(${mainBgDesktop2x});
     }
   }
 `;
@@ -116,24 +92,27 @@ export const Title = styled.h1`
 `;
 
 export const Subtitle = styled.p`
-  margin: 0 auto;
   display: flex;
-  margin-bottom: 44px;
-  text-align: center;
-  max-width: 305px;
 
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  max-width: 305px;
+  margin: 0 auto;
+  margin-bottom: 44px;
+
   font-size: ${({ theme }) => theme.fontSizes[2]};
   line-height: ${({ theme }) => theme.fontSizes[4]};
-
   letter-spacing: -0.02em;
   color: ${({ theme }) => theme.colors.white[200]};
+
+  text-align: center;
+
   @media screen and (min-width: ${theme.breakpoints[1]}) {
-    margin-bottom: 40px;
     max-width: 505px;
+    margin-bottom: 40px;
+
     font-size: ${({ theme }) => theme.fontSizes[4]};
     line-height: ${({ theme }) => theme.fontSizes[5]};
   }
+
   @media screen and (min-width: ${theme.breakpoints[2]}) {
     max-width: 540px;
   } ;
@@ -143,6 +122,7 @@ export const ButtonsWrapper = styled.div`
   display: flex;
   justify-content: center;
   gap: 12px;
+
   @media screen and (min-width: ${theme.breakpoints[1]}) {
     gap: 18px;
   } ;
@@ -152,37 +132,35 @@ export const LinkStyled = styled(Link)`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-family: ${({ theme }) => theme.fonts};
-  font-style: normal; 
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  font-size: ${({ theme }) => theme.fontSizes[2]};
-  text-decoration: none;
-  line-height: 21px;
+
   padding: 12px 24px;
 
-   @media screen and (min-width: ${theme.breakpoints[1]}) {
+  font-size: ${({ theme }) => theme.fontSizes[2]};
+  line-height: 21px;
+  text-decoration: none;
+
+  @media screen and (min-width: ${theme.breakpoints[1]}) {
     font-size: ${({ theme }) => theme.fontSizes[4]};
     line-height: ${({ theme }) => theme.fontSizes[5]};
     padding: 22px 45px;
-  };
+  }
 
   color: rgb(255, 255, 255);
-  
+
   background-color: ${({ look }) =>
     look === 'rounded' ? 'transparent' : '#8baa36'};
   border-radius: 24px 44px;
   border: ${({ look }) =>
     look === 'rounded' ? '1px solid rgb(250, 250, 250)' : 'none'};
 
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  
+  transition: ${({ theme }) =>
+    theme.transitions.create(['color', 'background-color', 'border-color'])};
+
   :hover,
   :focus {
     color: ${({ look }) => (look === 'rounded' ? '#8BAA36' : '#FAFAFA')};
     background-color: ${({ look }) =>
       look === 'rounded' ? 'transparent' : '#22252a'};
     border-color: ${({ look }) => (look === 'rounded' ? '#8BAA36' : 'none')};
-      }
-    }};
   }
 `;
