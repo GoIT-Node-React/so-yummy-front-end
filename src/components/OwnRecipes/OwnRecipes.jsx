6cdd31be2cec4258084a1e7ac0b1c332e1692632
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-09 07:37:28 +0300

    Add Own recipes page

---
+++ src/components/OwnRecipes/OwnRecipes.jsx
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from 'react';
+import {
+  OwnRecipesContainer,
+  OwnRecipesList,
+  OwnRecipesSection,
+  OwnRecipesTitle,
+} from './OwnRecipes.styled';
+import { getOwnRecipesService } from 'services/recipe.service';
+
+export default function OwnRecipes() {
+  const [recipesList, setRecipesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getRecipes = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const { data, limit, page, total } = await getOwnRecipesService();
+
+      setRecipesList(data.recipes);
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getRecipes();
+  }, [getRecipes]);
+
+  return (
+    <OwnRecipesSection>
+      <OwnRecipesContainer>
+        <OwnRecipesTitle>MyRecipes</OwnRecipesTitle>
+        <OwnRecipesList recipes={recipesList} />
+      </OwnRecipesContainer>
+    </OwnRecipesSection>
+  );
+}
