7b5067afc648109b4d699fa56994004d560de7cb
Lysak-Anna <hvlysak@gmail.com>
2023-04-09 14:36:49 +0300

    not-found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,1 +1,17 @@
-export default function NotFoundPage() {}
+import { Container } from 'components/common/Container.styled';
+import NotFound from '../../images/SVG/bro404/not-found.svg';
+import { BoldText, Image, Text, TextContainer } from './NotFoundPage.styled';
+
+export default function NotFoundPage() {
+  return (
+    <section>
+      <Container>
+        <Image src={NotFound} alt="Not found" />
+        <TextContainer>
+          <BoldText>We are sorry,</BoldText>
+          <Text>but the page you were looking for can't be found..</Text>
+        </TextContainer>
+      </Container>
+    </section>
+  );
+}
