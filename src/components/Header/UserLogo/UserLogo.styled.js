cec7bb64aab6610470664b527b8d7be9b779ed35
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-04-12 15:42:09 +0300

    Fix

--- src/components/Header/UserLogo/UserLogo.styled.js
+++ src/components/Header/UserLogo/UserLogo.styled.js
@@ -29,6 +29,8 @@ export const UserAvatarWrapper = styled.div`
 export const Avatar = styled.img`
   max-width: 100%;
   height: 100%;
+
+  object-fit: cover;
 `;
 
 export const UserName = styled.p`
